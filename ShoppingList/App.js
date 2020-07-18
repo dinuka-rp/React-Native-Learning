f163072ad0a7f0fe1cf7da85a83f80d91457c29c
DinDev3 <drpiyadigama@gmail.com>
2020-07-19 02:35:01 +0530

    Code Refactor

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, FlatList, Alert } from 'react-native';
 import { v4 as uuidv4 } from 'uuid';
+// import { uuid } from 'uuidv4';
 import Header from './components/Header';
 import ListItem from './components/ListItem'
 import AddItem from './components/AddItem'
-import { uuid } from 'uuidv4';
 
 const App = () => {
   const [items, setItems] = useState([
